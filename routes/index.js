60fb54d0137cc8729048c0dc68ad23fad0c90ca5
Sob <sobfred30@gmail.com>
2026-06-09 17:08:25 +0100

    modified email service to smt.gmail.com

--- routes/index.js
+++ routes/index.js
@@ -566,9 +566,11 @@ function getEventsMissingTablesResponse(res) {
   });
 }
 
-// Configure Nodemailer
+// Configure Nodemailer 
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE,
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
